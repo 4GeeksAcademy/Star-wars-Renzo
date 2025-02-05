f2e7dcf8c61ea06692ebb19b629a41813cb4346a
relo-ga <40379438+relo-ga@users.noreply.github.com>
2025-02-05 18:10:48 +0000

    starwars detalles de vista

--- src/js/component/personajesDetalle.jsx
+++ src/js/component/personajesDetalle.jsx
@@ -16,19 +16,32 @@ const PersonajesDetalles = () =>{
         let urlImagen = `https://starwars-visualguide.com/assets/img/characters/${props.item.uid}.jpg`
 
         return(
-            <div className="card mb-3" >
-            <div className="row g-0">
-                <div className="col-md-4">
-                <img src={urlImagen} className="img-fluid rounded-start" alt="..."/>
+            <div className="pt-5 bg-black bg-gradient">
+                <div className="card mb-3 mx-5" style={{border: "0"}} >
+                    <div className="row g-0">
+                        <div className="col-md-4">
+                            <img src={urlImagen} className="img-detalle img-fluid rounded-start" alt="..."/>
+                        </div>
+                        <div className="col-md-8 bg-dark bg-gradient" style={{}}>
+                            <div className="card-body">
+                                <h2 className="card-title text-white">{props.item.properties.name}</h2>
+                                <h5 className="card-text text-white">{props.item.description}</h5>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="col-md-8">
-                <div className="card-body">
-                    <h5 className="card-title">{props.item.properties.name}</h5>
-                    <p className="card-text">{props.item.description}</p>
-                </div>
+                <div className="d-flex text-center">
+                    <div className="col-4">
+                        <h3 style={{color:"#ffe81f"}}>Name: {props.item.properties.name} </h3>
+                    </div>
+                    <div className="col-4">
+                        <h3 style={{color:"#ffe81f"}}>Birth Year: {props.item.properties.birth_year} </h3>
+                    </div>
+                    <div className="col-4">
+                        <h3 style={{color:"#ffe81f"}}>height: {props.item.properties.height} </h3>
+                    </div>
                 </div>
             </div>
-        </div>
         )
     }
 
--- src/js/component/vehiculosDetalle.jsx
+++ src/js/component/vehiculosDetalle.jsx
@@ -16,19 +16,32 @@ const VehiculosDetalles = () =>{
         let urlImagen = `https://starwars-visualguide.com/assets/img/vehicles/${props.item.uid}.jpg`
 
         return(
-            <div className="card mb-3" >
-            <div className="row g-0">
-                <div className="col-md-4">
-                <img src={urlImagen} className="img-fluid rounded-start" alt="..."/>
+            <div className="pt-5 bg-black bg-gradient">
+                <div className="card mb-3 mx-5" style={{border: "0"}}>
+                    <div className="row g-0">
+                        <div className="col-md-4">
+                            <img src={urlImagen} className="img-detalle img-fluid rounded-start" alt="..."/>
+                        </div>
+                        <div className="col-md-8 bg-dark bg-gradient">
+                            <div className="card-body">
+                                <h2 className="card-title text-white">{props.item.properties.name}</h2>
+                                <h5 className="card-text text-white">{props.item.description}</h5>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="col-md-8">
-                <div className="card-body">
-                    <h5 className="card-title">{props.item.properties.name}</h5>
-                    <p className="card-text">{props.item.description}</p>
-                </div>
+                <div className="d-flex text-center">
+                    <div className="col-4">
+                        <h3 style={{color:"#ffe81f"}}>Name: {props.item.properties.name} </h3>
+                    </div>
+                    <div className="col-4">
+                        <h3 style={{color:"#ffe81f"}}>Passengers: {props.item.properties.passengers} </h3>
+                    </div>
+                    <div className="col-4">
+                        <h3 style={{color:"#ffe81f"}}>Cost: {props.item.properties.cost_in_credits} </h3>
+                    </div>
                 </div>
             </div>
-        </div>
         )
     }
 
